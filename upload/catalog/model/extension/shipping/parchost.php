ea38ad04e28502efe1e0843a11090d04ad208ff5
Mesh Manuel <meshmmanuel@yahoo.com>
2020-10-09 14:01:56 +0100

    Added parchost api

--- upload/catalog/model/extension/shipping/parchost.php
+++ upload/catalog/model/extension/shipping/parchost.php
@@ -31,7 +31,7 @@ class ModelExtensionShippingParchost extends Model
 
 				$quote_data['parchost_' . $location['id']] = array(
 					'code'         => 'parchost.parchost_' . $location['id'],
-					'title'        =>  $location['location'],
+					'title'        =>  $location['title'],
 					'cost'         => $location['cost'],
 					'tax_class_id' => 0,
 					'text'         => $this->currency->format($location['cost'], $this->session->data['currency'])
@@ -53,119 +53,21 @@ class ModelExtensionShippingParchost extends Model
 	public function getLocations($user_coordinates)
 	{
 		$available = [];
-		$data = json_decode('{
-			"parchosts": [
-			  {
-				"id": 1,
-				"title": "Ikeja Shopping Mall - Parchost",
-				"cost": "3.01",
-				"location": "Ikeja",
-				"long":"1.23444",
-				"lat":"2.34555"
-			  },
-			  {
-				"id": 2,
-				"title": "Allen Avenue - Parchost",
-				"cost": "9.14",
-				"location": "Ikeja",
-				"long":"1.56444",
-				"lat":"2.345456"
-			  },
-			  {
-				"id": 3,
-				"title": "National Stadium - Parchost",
-				"cost": "7.78",
-				"location": "Surulere",
-				"long":"1.673444",
-				"lat":"2.346345"
-			  },
-			  {
-				"id": 4,
-				"title": "Magodo Phase 1 - Parchost",
-				"cost": "4.81",
-				"location": "Magodo",
-				"long":"1.83444",
-				"lat":"2.83243"
-			  },
-			  {
-				"id": 5,
-				"title": "Lekki Tollgate - Parchost",
-				"cost": "9.64",
-				"location": "Lekki",
-				"long":"1.90444",
-				"lat":"2.45946"
-			  },
-			  {
-				"id": 6,
-				"title": "Lekki Conservation Center - Parchost",
-				"cost": "8.51",
-				"location": "Lekki",
-				"long":"1.9999",
-				"lat":"2.4564565"
-			  }
-			]
-		  }');
-
-		// Find Closesth Parchost
-		$closestParchosts = $this->findClosestParchost($data->parchosts, $user_coordinates);
-
-		// fetch parchost
-		foreach ($data->parchosts as $parchost) {
-			if (isset($closestParchosts[$parchost->id])) {
-				array_push($available, ['id' => $parchost->id, 'location' => $parchost->title, 'cost' => $parchost->cost]);
-			}
-		}
-		return $available;
+		$data = $this->getClosestParchost($user_coordinates['lat'], $user_coordinates['long']);
+		return $data;
 	}
 
-	private function findClosestParchost($parchosts, $user_coordinates)
+	protected function getClosestParchost($lat, $long)
 	{
-		$closest = [];
-		foreach ($parchosts as $key => $parchost) {
-			$disatance = $this->getDistance($user_coordinates['lat'], $user_coordinates['long'], $parchost->lat, $parchost->long);
-			$closest[$parchost->id] = $disatance;
-		}
-		asort($closest);
-		return array_slice($closest, 0, 3, true);
-	}
+		$cURLConnection = curl_init('https://api.parchost.com/parchost/closest?lat=' . $lat . '&long=' . $long);
+		curl_setopt($cURLConnection, CURLOPT_RETURNTRANSFER, true);
+		$apiResponse = curl_exec($cURLConnection);
+		curl_close($cURLConnection);
 
+		// $apiResponse - available data from the API request
+		$jsonArrayResponse = json_decode($apiResponse, true);
 
-	/*::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
-	/*::                                                                         :*/
-	/*::  This routine calculates the distance between two points (given the     :*/
-	/*::  latitude/longitude of those points). It is being used to calculate     :*/
-	/*::  the distance between two locations using GeoDataSource(TM) Products    :*/
-	/*::                                                                         :*/
-	/*::  Definitions:                                                           :*/
-	/*::    South latitudes are negative, east longitudes are positive           :*/
-	/*::                                                                         :*/
-	/*::  Passed to function:                                                    :*/
-	/*::    lat1, lon1 = Latitude and Longitude of point 1 (in decimal degrees)  :*/
-	/*::    lat2, lon2 = Latitude and Longitude of point 2 (in decimal degrees)  :*/
-	/*::                                                                         :*/
-	/*::  Worldwide cities and other features databases with latitude longitude  :*/
-	/*::  are available at https://www.geodatasource.com                          :*/
-	/*::                                                                         :*/
-	/*::  For enquiries, please contact sales@geodatasource.com                  :*/
-	/*::                                                                         :*/
-	/*::  Official Web site: https://www.geodatasource.com                        :*/
-	/*::                                                                         :*/
-	/*::         GeoDataSource.com (C) All Rights Reserved 2018                  :*/
-	/*::                                                                         :*/
-	/*::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
-	protected function getDistance($lat1, $lon1, $lat2, $lon2)
-	{
-		if (($lat1 == $lat2) && ($lon1 == $lon2)) {
-			return 0;
-		} else {
-			$theta = $lon1 - $lon2;
-			$dist = sin(deg2rad($lat1)) * sin(deg2rad($lat2)) +  cos(deg2rad($lat1)) * cos(deg2rad($lat2)) * cos(deg2rad($theta));
-			$dist = acos($dist);
-			$dist = rad2deg($dist);
-			$miles = $dist * 60 * 1.1515;
-			// return in kilo meters
-			return $miles * 1.609344;
-		}
+		return $jsonArrayResponse['data'];
 	}
 
 	private function getCoordinatesForAddress($address)
